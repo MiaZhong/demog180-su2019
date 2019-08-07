85d038f15e644de15db103cfb6c5b1110e2d85a3
Mia-Zhong <miazhongrj@gmail.com>
2019-08-06 22:18:21 -0700

    hw4

---
+++ hwk/hw4/tests/q3.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'q3',
+  'points': 2,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> test_bc[1]
+          0.0
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> test_bc[6]
+          2.0
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
